5e0d3808af22819b405e0c3476e9af09b3eeb1d9
Tracy <119116409+tracycute@users.noreply.github.com>
2024-06-06 18:52:30 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title('This is a next level SEO app')
+st.write('Make your idea real')
+st.write('What is your seed keyword?')
+st.text_input(label = '', value = "", label_visibility='hidden')
