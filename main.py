2d110ac00a57e0f249dda899d14947496834de4c
Tracy <119116409+tracycute@users.noreply.github.com>
2024-06-06 19:43:39 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ import streamlit as st
 st.title('This is a next level SEO app')
 st.write('Make your idea real')
 st.write('What is your seed keyword?')
-st.text_input(label = '', value = "", label_visibility='hidden')
+st.text_input(label = '', value = '', label_visibility='hidden')
+
+#https://21520255-cau-1-2.streamlit.app/
